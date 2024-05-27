4616f3b9b46a3f1c587087c64139e40a7b21db71
iam-farhin <mirzafarhin6202@gmail.com>
2024-05-27 12:21:58 +0530

    Addedsetup for CloudFront

--- Farhin_Cloud_Resume/iac/main.tf
+++ Farhin_Cloud_Resume/iac/main.tf
@@ -25,6 +25,10 @@ resource "aws_s3_bucket" "farhin_website_bucket" {
     Value = "bill_costs"
   } 
  } 
+
+ resource "aws_cloudfront_origin_access_identity" "oai" {
+    comment = "OAI for s3 bucket"
+ }
  
  resource "aws_s3_bucket_policy" "website_bucket_policy" {
  bucket = aws_s3_bucket.farhin_website_bucket.id
@@ -39,5 +43,53 @@ resource "aws_s3_bucket" "farhin_website_bucket" {
         }
     ]
  })
+ }
+
+ resource "aws_cloudfront_distribution" "s3_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.farhin_website_bucket.bucket_regional_domain_name
+        origin_id = "s3-${aws_s3_bucket.farhin_website_bucket.id}"
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+        }
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "s3-${aws_s3_bucket.farhin_website_bucket.id}"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true 
+    }
+
+    tags = {
+    Name = "Billing-Monitor"
+    Value = "bill_costs"
+  } 
  }
 
